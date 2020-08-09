f6597f27bdbd0ea2c0db2047511e0c6ddef5c744
hugosrc <hugo.uibai54@gmail.com>
2020-08-09 11:03:20 -0300

    Initial Structure

---
+++ src/utils/ts-module-alias.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+import moduleAlias from 'module-alias'
+
+const files = path.resolve(__dirname, '../..')
+
+moduleAlias.addAliases({
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test')
+})
