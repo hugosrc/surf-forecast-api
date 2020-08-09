ae8a5f7124648103aabe580661a8f4c0a890b14b
hugosrc <hugo.uibai54@gmail.com>
2020-08-09 12:28:42 -0300

    feat: create Server and functional test for forecast

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './utils/ts-module-alias'
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
+
+export class SetupServer extends Server {
+
+  constructor (private port = 3000) {
+    super()
+  }
+
+  public init(): void {
+    this.setupExpress()
+    this.setupControllers()
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json())
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController()
+    this.addControllers([forecastController])
+  }
+
+  public getApp(): Application {
+    return this.app
+  }
+
+}
---
+++ test/jest-setup.ts
@@ -0,0 +1,10 @@
+import supertest from 'supertest'
+import { SetupServer } from '@src/server'
+
+beforeAll(() => {
+  const server = new SetupServer()
+
+  server.init()
+
+  global.testRequest = supertest(server.getApp())
+})
