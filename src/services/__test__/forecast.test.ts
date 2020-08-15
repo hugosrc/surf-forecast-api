1f325479a1fa2c6932920533ff91ad9eb8bb0d7d
hugosrc <hugo.uibai54@gmail.com>
2020-08-15 18:49:30 -0300

    feat: refactor forecast controller

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,11 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
-import {
-  Beach,
-  BeachPosition,
-  Forecast,
-  ForecastProcessingInternalError,
-} from '../forecast';
+import { Forecast, ForecastProcessingInternalError } from '../forecast';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -23,7 +19,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id',
       },
     ];
 
@@ -110,7 +105,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id',
       },
     ];
 
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -33,7 +33,7 @@ describe('Beaches functional tests', () => {
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         error:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+          'Beach validation failed: lat: Cast to Number failed for value "invalid-string" at path "lat"',
       });
     });
   });
