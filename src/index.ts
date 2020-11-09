ccfc7ce0d33781736cba636adce09538b5234bc9
hugosrc <hugo.uibai54@gmail.com>
2020-11-09 18:34:59 -0300

    feat: adds Graceful shutdown and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
 import { SetupServer } from './server';
 import config from 'config';
+import logger from './logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+  );
+
+  throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
+  try {
+    const server = new SetupServer(config.get('App.port'));
 
-  await server.init();
+    await server.init();
 
-  server.start();
+    server.start();
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          logger.info('App exited with success');
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
