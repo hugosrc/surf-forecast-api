032e42eef5abd43a933bdb7b395cf6cd650da4ad
hugosrc <hugosr.contato@gmail.com>
2021-08-21 08:21:00 -0300

    style: add white line to end of api-error.spec.ts file

--- src/utils/errors/__tests__/api-error.spec.ts
+++ src/utils/errors/__tests__/api-error.spec.ts
@@ -3,7 +3,7 @@ import ApiError from '../api-error';
 describe('ApiError', () => {
   it('should format error with mandatory fields', () => {
     const error = ApiError.format({ code: 404, message: 'User not found!' });
-    
+
     expect(error).toEqual({
       message: 'User not found!',
       error: 'Not Found',
